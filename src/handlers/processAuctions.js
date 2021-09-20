5c9c0d267b9cc0484beefa136bb405590476e441
DanielMarquesz <daniel.marques142@gmail.com>
2021-09-19 21:39:56 -0300

    feat: close auctions when they are open

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,18 @@
-import { getEndedAuctions } from '../lib/getEndedAuction'
+import createError from 'http-errors';
+import { closeAuction } from '../lib/closeAuction';
+import { getEndedAuctions } from '../lib/getEndedAuction';
 
 async function processAuctions(event, context) {
-  const auctionsClose = await getEndedAuctions();
-  console.log(auctionsClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+    await Promise.all(closePromises);
+
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.log(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
